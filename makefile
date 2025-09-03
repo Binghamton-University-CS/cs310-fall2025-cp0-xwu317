60db43f8f17ea213352c53569853cce7dd9403a8
xwu19 <xwu19@cs.binghamton.edu>
2025-09-02 21:26:00 -0400

    final commiti

--- makefile
+++ makefile
@@ -6,16 +6,20 @@ all: zoo
 
 # Specify the object files that the target depends on
 # Also specify the object files needed to create the executable
-zoo: zoo.o
-	g++ zoo.o -o zoo
-	
+zoo: zoo.o Animal.o AnimalsInZoo.o
+	g++ zoo.o Animal.o AnimalsInZoo.o -o zoo
 
 # Specify how the object files should be created from source files
-zoo.o: zoo.cpp
+zoo.o: zoo.cpp Animal.h AnimalsInZoo.h
 	g++ -Wall -Wextra -c zoo.cpp
-	
+
+Animal.o: Animal.cpp Animal.h
+	g++ -Wall -Wextra -c Animal.cpp
+
+AnimalsInZoo.o: AnimalsInZoo.cpp AnimalsInZoo.h Animal.h
+	 g++ -Wall -Wextra -c AnimalsInZoo.cpp
 
 # Specify the object files and executables that are generated
 # and need to be removed to re-compile the whole thing
 clean:
-	rm -f *.o zoo
+	rm -f *.o zoo Animal AnimalsInZoo
--- zoo.cpp
+++ zoo.cpp
@@ -2,33 +2,25 @@
 #include <stdlib.h>
 using namespace std;
 
-class Animal {
-   public:
-      Animal(string speciesName, unsigned int discoveryYear) {
-         species = speciesName;
-         year_discovered = discoveryYear;
-      }
-
-      Animal() : species(""), year_discovered(0) {};
-
-      void display() {
-         cout << species << " [" << year_discovered << "]" << endl;
-      }
-
-   private:
-      string species = "";
-      unsigned int year_discovered = 0;
-};
+#include "Animal.h"
+#include "AnimalsInZoo.h"
 
 int main() {
    Animal *animal1 = new Animal("African Elephant", 1758);
    Animal animal2("Giant Panda", 1869);
+   Animal *animal3 = new Animal("Maldvies Damselfish", 2024);
+
+   AnimalsInZoo numOfAnimals2(animal2);
 
    delete animal1;
    animal1 = new Animal("Snow Leopard", 1777);
 
-   animal2.display();
+   animal2.display()
+   numOfAnimals2.display();
+   
    animal1->display();
+
+   animal3->display();
 
    delete animal1;
 }
